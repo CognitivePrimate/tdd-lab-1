5332ae0d62387d813602bf29e2b2c002a592e62b
Derek Jackson <d4jackson84@gmail.com>
2021-06-10 16:15:17 -0400

    fixed functionality for punctuation catches

--- script.js
+++ script.js
@@ -1,55 +1,71 @@
 const translate = (str) => {
     const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-    const closingPunctuation = ["!", ".", "?", "!!!", "...",]
-
+    const closingPunctuation = ["!", ".", "?", "!!!", "...",];
+    let hasPunctuation = false;
+    let newStr = "";
     // commented out below to meet conflicting build spec & extended challenges case-sensitivity
     // str = str.toLowerCase();
-    let newStr = "";
-    let punctuationIndex;
+    
+    
 
     // punctuation check function -- moves punctuation to last index
     let closingPuncuationCheck = (str) => {
         let split = str.split("");
-        console.log(split);
         for (let char of str){
             if (closingPunctuation.includes(char)){
+                let split = str.split("");
                 let punctuationIndex = split.indexOf(char);
                 split.push(split.splice(punctuationIndex, 1)[0]);
                 const backToString = split.join("");
                 let newStr = backToString;
-                console.log(newStr);
                 return newStr;
-                break;
-            };
+            }; 
         };   
     }
     
     // non vowels will equal -1
     if (vowels.indexOf(str[0]) > -1){
         newStr = `${str}way`;
-        console.log(newStr)
-        // WHY WONT THIS WORK??
-        closingPuncuationCheck(newStr);
+        // console.log(newStr)
 
-        console.log(newStr)
+        for (let char of str){
+            if (closingPunctuation.includes(char)){
+                newStr = closingPuncuationCheck(newStr);
+                // console.log(newStr);
+                continue;
+            }
+        }
+
         return newStr;
+         
     } else if (vowels.indexOf(str[0]) == -1){
+        let noPunct = () => `${str.slice(vowelIndex) + str.slice(0, vowelIndex)}ay`;
         for (let char of str){
             if (vowels.includes(char)){
                 vowelIndex = str.indexOf(char);
+                console.log(vowelIndex);
                 break;
             };
         };
-        console.log(vowelIndex);
-        return `${str.slice(vowelIndex) + str.slice(0, vowelIndex)}ay`;
-        
-    }
 
-    
-    
+        // if punctuation is found in consonant starting string
+        for (let char of str){
+            if (closingPunctuation.includes(char)){
+                hasPunctuation = true;
+                str = `${str}`;
+                str = noPunct();
+                str = closingPuncuationCheck(str)
+                break;
+            };
+        };
+
+        // final result returns when word starts with consonant but result may vary based on punctuation
+        return hasPunctuation === true ? str : noPunct();
+        
+    };
 }
 
-// console.log(translate("al?lo"));
+console.log(translate("gira!ffe"));
 
 module.exports = {
     translate
